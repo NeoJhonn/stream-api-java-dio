6405b2cb83f4b91bca6f6d01f54e85e9e32c0cc1
NeoJhonn <jhonny.azevedo@gmail.com>
2024-05-21 00:57:24 -0300

    Exemplo Consumer feito

---
+++ src/functional_interface/examples/ConsumerExample.java
@@ -0,0 +1,38 @@
+package functional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        // Lista de números
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // Usar um Consumer com expressão lambda para imprimir números ímpares
+        Consumer<Integer> imprimirNumerosImparesLambda = numero -> {
+          if (numero % 2 != 0) {
+              System.out.print(numero + ", ");
+          }
+        };
+
+        // Usar um Consumer com uma classe anônima para imprimir números ímpares
+        Consumer<Integer> imprimirNumerosImparesClasseAnonima = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer numero) {
+                if (numero % 2 != 0) {
+                    System.out.print(numero + ", ");
+                }
+            }
+        };
+
+        // usar o Consumer com classe anônima para imprimir números ímpares no Stream
+        numeros.stream().forEach(imprimirNumerosImparesClasseAnonima);
+
+        System.out.println();
+
+        // usar o Consumer com lambda para imprimir números ímpares no Stream
+        numeros.stream().forEach(imprimirNumerosImparesLambda);
+    }
+}
